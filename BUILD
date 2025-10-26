81b7749014db69450f50faed264769511b51a55e
Tom Dyas <tom@shoalsoftware.com>
2025-10-25 17:21:47 -0700

    adapt to environment better
    1. Use the `package_shell_command` available on Pants v2.30.x to avoid the need to make a `pkg-bin` target to trigger compilation.
    
    2. Use the regular PATH to find `go` and derive the `GOROOT` by just asing `go` directly.

--- BUILD
+++ BUILD
@@ -3,3 +3,13 @@ shoalsoft_go_tests()
 experimental_workspace_environment(
     name="workspace",
 )
+
+shell_command(
+    name="find_goroot",
+    command='echo >go.env "GOENV=$(go env GOROOT)"',
+    output_files=["go.env"],
+    path_env_modify="off",
+    extra_env_vars=[
+        "PATH",
+    ]
+)
--- pants-macros/golang.py
+++ pants-macros/golang.py
@@ -1,42 +1,52 @@
 # Macros to support building Go sources using the Go SDK run in a workspace environment.
 
-# Location of the Go SDK's GOROOT. 
-# TODO: Make this SDK location more configurable?
-GOROOT = "/usr/local/Cellar/go/1.25.1/libexec"
-
 # Declares that this directory contains a Go binary to be built. The binary will
 # be copied under dist/ with the `dist_bin_name`.
 def shoalsoft_go_binary(
     dist_bin_name: str = "bin",
     workspace_env_name: str = "__local_workspace__"
 ) -> None:
-    # Define a `shell_command` target to run in the workspace and build the binary. This
-    # target is separate so that we can pass in `extra_env_vars` which `run_shell_command`
-    # does not support (except if set directly in the command).
-    shell_command(
+    if "/" in dist_bin_name:
+        raise ValueError(f"The `dist_bin_name` field cannot contain the `/`  path separator.")
+
+    # Define a target to run in the workspace and build the binary. If Pants supports `package_shell_command`, then
+    # it is used for the binary's target. On older Pants versions, two targets are made instead: a `shell_command`
+    # to build the binary and a `run_shell_command` target to trigger the build.
+    if "package_shell_command" in globals():
+        target_type = package_shell_command
+        create_pkg_bin_target = False
+    else:
+        target_type = shell_command
+        create_pkg_bin_target = True
+
+    # Build the binary and copy the output to the "chroot" for capture.
+    target_type(
         name="bin",
-        # Build the binary and copy the output to the "chroot" for capture.
-        command="go build -o ./bin && cp ./bin {chroot}/bin",
-        output_files=["bin"],
+        command=f"source {{chroot}}/go.env && go build -o './{dist_bin_name}' && cp './{dist_bin_name}' '{{chroot}}/{dist_bin_name}'",
+        execution_dependencies=[
+            "//:find_goroot",
+        ],
+        output_files=[dist_bin_name],
         workdir=".",
+        path_env_modify="off",
         extra_env_vars=[
-            f"GOROOT={GOROOT}",
-            f"PATH={GOROOT}/bin:/usr/local/bin:/usr/bin:/bin",
+            "PATH",
             "HOME",
         ],
         environment=workspace_env_name,
         log_output=True,
     )
 
-    # Pants does not currently have a way to trigger the shell backend in response to a `package` goal. The work-around
-    # is for users to invoke the `run` goal with this `:pkg-bin` target to (indirectly) build the binary and copy
-    # it to the dist directory using the provided `dist_bin_name`.
-    run_shell_command(
-        name="pkg-bin",
-        command=f"mkdir -p ./dist && cp {{chroot}}/bin ./dist/{dist_bin_name}",
-        workdir="/",
-        execution_dependencies=[":bin"],
-    )
+    # Older versions of Pants do not have a way to trigger the shell backend in response to a `package` goal.
+    # The work-around is for users to invoke the `run` goal with this `:pkg-bin` target to (indirectly) build
+    # the binary and copy it to the dist directory using the provided `dist_bin_name`.
+    if create_pkg_bin_target:
+        run_shell_command(
+            name="pkg-bin",
+            command=f"mkdir -p ./dist && cp {{chroot}}/{dist_bin_name} ./dist/{dist_bin_name}",
+            workdir="/",
+            execution_dependencies=[":bin"],
+        )
 
 
 # Declares a top-level `go_tests` target to run all Go tests in a module.
@@ -45,11 +55,14 @@ def shoalsoft_go_tests(
 ) -> None:
     test_shell_command(
         name="go_tests",
-        command="go test ./...",
+        command="source {chroot}/go.env && go test ./...",
+        execution_dependencies=[
+            "//:find_goroot",
+        ],
         workdir=".",
+        path_env_modify="off",
         extra_env_vars=[
-            f"GOROOT={GOROOT}",
-            f"PATH={GOROOT}/bin:/usr/local/bin:/usr/bin:/bin",
+            "PATH",
             "HOME",
         ],
         environment=workspace_env_name,
